32654bc5284e8f696f4c718bb998bb457714ea69
depit <depit@SuSe>
2015-09-11 21:36:30 +0300

    2015-09.12_SpringMvcFormHandling-codejava

---
+++ SpringMvcFormHandling-codejava/src/main/java/net/codejava/spring/controller/RegisterController.java
@@ -0,0 +1,47 @@
+package net.codejava.spring.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import net.codejava.spring.model.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/register")
+public class RegisterController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String viewRegistration(Map<String, Object> model) {
+		User userForm = new User();		
+		model.put("userForm", userForm);
+		
+		List<String> professionList = new ArrayList<>();
+		professionList.add("Developer");
+		professionList.add("Designer");
+		professionList.add("IT Manager");
+		model.put("professionList", professionList);
+		
+		return "Registration";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String processRegistration(@ModelAttribute("userForm") User user, 
+			Map<String, Object> model) {
+		
+		// implement your own registration logic here...
+		
+		// for testing purpose:
+		System.out.println("username: " + user.getUsername());
+		System.out.println("password: " + user.getPassword());
+		System.out.println("email: " + user.getEmail());
+		System.out.println("birth date: " + user.getBirthDate());
+		System.out.println("profession: " + user.getProfession());
+		
+		return "RegistrationSuccess";
+	}
+}
---
+++ SpringMvcFormHandling-codejava/src/main/java/net/codejava/spring/model/User.java
@@ -0,0 +1,52 @@
+package net.codejava.spring.model;
+
+import java.util.Date;
+
+public class User {
+	private String username;
+	private String password;
+	private String email;
+	private Date birthDate;
+	private String profession;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getProfession() {
+		return profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+
+}
